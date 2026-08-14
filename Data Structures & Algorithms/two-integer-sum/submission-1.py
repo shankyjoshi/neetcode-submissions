c173813cd776ebf9f4fe595eaef6cdbd41846ebd
Shashank Joshi <54698405+shankyjoshi@users.noreply.github.com>
2026-08-13 22:59:07 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # run a loop over all numbers
+        # starting with first number and adding rest of the numbers to get to target sum 
+        # retrun the answer with smallest index first
+
+
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j]== target:
+        #             return [i,j]
+        
+        temp = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in temp:
+                return [temp[target-nums[i]], i]
+            temp[nums[i]] = i
+    
+        return [-1, -1]
